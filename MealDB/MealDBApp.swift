8e0ec32f74565f728d3d5ab8262163c9d6a94881
IkerPerea <ikerpereatrejo@gmail.com>
2024-08-22 21:58:36 +0200

    Initial Commit

---
+++ MealDB/MealDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealDBApp.swift
+//  MealDB
+//
+//  Created by Iker Perea on 22/8/24.
+//
+
+import SwiftUI
+
+@main
+struct MealDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
